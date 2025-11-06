66bf90c0cac73d8538f97e9cd5926a52b810a8a4
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2025-11-07 03:02:46 +0600

    fix: remove unused import for signup_screen in app.dart

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,7 +1,6 @@
 import 'package:cardinal_quotes_app/src/features/main/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'constants/app_theme.dart';
-import 'features/authentication/screens/signup_screen.dart';
 
 class CardinalQuotesApp extends StatelessWidget {
   const CardinalQuotesApp({super.key});
