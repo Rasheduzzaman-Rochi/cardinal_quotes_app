9f10f5a08b2819a2884eb64872ccc1e52d7f305d
rasheduzzamanrochi <rasheduzzaman2003@gmail.com>
2025-11-08 12:31:12 +0600

    feat: add Podfile and update xcconfig files for iOS project setup; integrate ScreenUtil for responsive design

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:cardinal_quotes_app/src/features/main/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'constants/app_theme.dart';
 
 class CardinalQuotesApp extends StatelessWidget {
@@ -7,11 +8,20 @@ class CardinalQuotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Cardinal Quotes',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme,
-      home: const MainScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(412, 915),
+      minTextAdapt: true,
+      splitScreenMode: true,
+
+      builder: (context, child) {
+        return MaterialApp(
+          title: 'Cardinal Quotes',
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme.lightTheme,
+          home: child,
+        );
+      },
+      child: const MainScreen(),
     );
   }
 }
